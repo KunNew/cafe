509112e07f0c892ca1a02f16059817bce6825ee4
pheak <kunnew20@gmail.com>
2023-02-02 09:39:16 +0700

    update profile

--- layouts/guest.vue
+++ layouts/guest.vue
@@ -1,10 +1,16 @@
 <template>
-  <div>
-    <div style="position: fixed; right: 10px; z-index: 9999;">
-                <notification v-for="notification in $store.state.notifications" :key="notification.id" :item="notification"></notification>
-            </div>
-    <Nuxt />
-  </div>
+  <v-app>
+    <div style="position: fixed; right: 10px; z-index: 9999">
+      <notification
+        v-for="notification in $store.state.notifications"
+        :key="notification.id"
+        :item="notification"
+      ></notification>
+    </div>
+<v-main>
+  <Nuxt />
+</v-main>
+  </v-app>
 </template>
 
 <script>
--- pages/setting.vue
+++ pages/setting.vue
@@ -20,10 +20,15 @@
         </v-tab>
         <v-tab>
           <v-icon left>mdi-account</v-icon>
-            Change password
+          Change password
         </v-tab>
         <v-tab-item>
-          <v-form @submit.prevent="updateProfile" ref="formProfile" lazy-validation :disabled="formProfile.submitting">
+          <v-form
+            @submit.prevent="updateProfile"
+            ref="formProfile"
+            lazy-validation
+            :disabled="formProfile.submitting"
+          >
             <v-card flat>
               <v-card-text>
                 <v-row>
@@ -34,57 +39,62 @@
                     </input-image-crop> -->
                   </v-col>
                   <v-col cols="12" class="py-0">
-
                     <v-text-field
-                        autocomplete="off"
-                        label="Last Name"
-                        name="last_name"
-                        v-model="formProfile.data.last_name"
-                        required
-                        :rules="[v => !!v || 'Last Name is required']"
-                        outlined>
+                      autocomplete="off"
+                      label="Last Name"
+                      name="last_name"
+                      v-model="formProfile.data.last_name"
+                      required
+                      :rules="[(v) => !!v || 'Last Name is required']"
+                      outlined
+                    >
                     </v-text-field>
                   </v-col>
                   <v-col cols="12" class="py-0">
-
                     <v-text-field
-                        autocomplete="off"
-                        label="First Name"
-                        name="first_name"
-                        v-model="formProfile.data.first_name"
-                        required
-                        :rules="[v => !!v || 'First Name is required']"
-                        outlined>
+                      autocomplete="off"
+                      label="First Name"
+                      name="first_name"
+                      v-model="formProfile.data.first_name"
+                      required
+                      :rules="[(v) => !!v || 'First Name is required']"
+                      outlined
+                    >
                     </v-text-field>
                   </v-col>
                   <v-col cols="12" class="py-0">
-
                     <v-text-field
-                        autocomplete="off"
-                        label="Username"
-                        name="username"
-                        v-model="formProfile.data.username"
-                        required
-                        :rules="[v => !!v || 'Username is required']"
-                        outlined>
+                      autocomplete="off"
+                      label="Username"
+                      name="username"
+                      v-model="formProfile.data.username"
+                      required
+                      :rules="[(v) => !!v || 'Username is required']"
+                      outlined
+                    >
                     </v-text-field>
                   </v-col>
                   <v-col cols="12" class="py-0">
-
                     <v-text-field
-                        autocomplete="off"
-                        label="Email"
-                        name="email"
-                        v-model="formProfile.data.email"
-                        prepend-inner-icon="mdi-email"
-                        type="email"
-                        required
-                        :rules="[v => !!v || 'Email is requred']"
-                        outlined>
+                      autocomplete="off"
+                      label="Email"
+                      name="email"
+                      v-model="formProfile.data.email"
+                      prepend-inner-icon="mdi-email"
+                      type="email"
+                      required
+                      :rules="[(v) => !!v || 'Email is requred']"
+                      outlined
+                    >
                     </v-text-field>
                   </v-col>
                   <v-col cols="12">
-                    <v-btn type="submit" :loading="formProfile.submitting" color="success">Save</v-btn>
+                    <v-btn
+                      type="submit"
+                      :loading="formProfile.submitting"
+                      color="success"
+                      >Save</v-btn
+                    >
                   </v-col>
                 </v-row>
               </v-card-text>
@@ -94,183 +104,233 @@
         <v-tab-item>
           <v-card flat>
             <v-card-text>
-              <v-form @submit.prevent="changePassword" lazy-validation ref="formPassword" :disabled="formPassword.submitting">
+              <v-form
+                @submit.prevent="changePassword"
+                lazy-validation
+                ref="formPassword"
+                :disabled="formPassword.submitting"
+              >
                 <v-row>
                   <v-col cols="12" class="py-0">
                     <v-text-field
-                        v-model="formPassword.data.old_password"
-                        autocomplete="off"
-                        label="Old Passwrod"
-                        prepend-inner-icon="mdi-lock"
-                        required
-                        :rules="[v => !!v || 'Old password is requred']"
-                        type="password"
-                        outlined>
+                      v-model="formPassword.data.old_password"
+                      autocomplete="off"
+                      label="Old Passwrod"
+                      prepend-inner-icon="mdi-lock"
+                      required
+                      :rules="[(v) => !!v || 'Old password is requred']"
+                      type="password"
+                      outlined
+                    >
                     </v-text-field>
                   </v-col>
                   <v-col cols="12" class="py-0">
                     <v-text-field
-                        v-model="formPassword.data.password"
-                        autocomplete="off"
-                        label="New Password"
-                        prepend-inner-icon="mdi-lock"
-                        required
-                        :rules="[v => !!v || 'New password', v => (v && v.length >= 8) ||  'Password must be at least 8']"
-                        type="password"
-                        outlined>
+                      v-model="formPassword.data.password"
+                      autocomplete="off"
+                      label="New Password"
+                      prepend-inner-icon="mdi-lock"
+                      required
+                      :rules="[
+                        (v) => !!v || 'New password',
+                        (v) =>
+                          (v && v.length >= 8) || 'Password must be at least 8',
+                      ]"
+                      type="password"
+                      outlined
+                    >
                     </v-text-field>
                   </v-col>
                   <v-col cols="12" class="py-0">
                     <v-text-field
-                        v-model="formPassword.data.confirm_password"
-                        autocomplete="off"
-                        label="Confirm Password"
-                        prepend-inner-icon="mdi-lock"
-                        required
-                        :rules="confirmPasswordRule"
-                        type="password"
-                        outlined>
+                      v-model="formPassword.data.confirm_password"
+                      autocomplete="off"
+                      label="Confirm Password"
+                      prepend-inner-icon="mdi-lock"
+                      required
+                      :rules="confirmPasswordRule"
+                      type="password"
+                      outlined
+                    >
                     </v-text-field>
                   </v-col>
                   <v-col cols="12">
-                    <v-btn type="submit" ref="btn_change_password" :loading="formPassword.submitting" color="success">Save</v-btn>
+                    <v-btn
+                      type="submit"
+                      ref="btn_change_password"
+                      :loading="formPassword.submitting"
+                      color="success"
+                      >Save</v-btn
+                    >
                   </v-col>
                 </v-row>
-                </v-form>
+              </v-form>
             </v-card-text>
           </v-card>
         </v-tab-item>
       </v-tabs>
     </v-card>
   </v-container>
-  </template>
+</template>
 
-  <script>
-  import { mapGetters } from 'vuex'
-  import axiosApiInstance from '~/utils/utilites'
-  export default {
-      data: () => ({
-        dialog: null,
-        tab: null,
+<script>
+import { mapGetters } from 'vuex'
+import axiosApiInstance from '~/utils/utilites'
+export default {
+  data: () => ({
+    dialog: null,
+    tab: null,
 
-        formProfile: {
-          submitting: false,
-          data: {
-            avatar: {
-              src: '',
-              file: null
-            },
-            first_name: '',
-            last_name: '',
-            username: '',
-            email: '',
-          }
+    formProfile: {
+      submitting: false,
+      data: {
+        avatar: {
+          src: '',
+          file: null,
         },
-
-        formPassword: {
-          submitting: false,
-          data: {
-            old_password: '',
-            password: '',
-            confirm_password: '',
-          }
-        }
-      }),
-
-      computed: {
-        ...mapGetters('auth', {
-          user: 'user'
-        }),
-
-        confirmPasswordRule()
-        {
-          return [
-            v => !!v || 'Confirm Password',
-            () => this.formPassword.data.password == this.formPassword.data.confirm_password || 'Password does not match'
-          ]
-        }
+        first_name: '',
+        last_name: '',
+        username: '',
+        email: '',
       },
+    },
 
-      mounted()
-      {
-        this.fetchProfile()
+    formPassword: {
+      submitting: false,
+      data: {
+        old_password: '',
+        password: '',
+        confirm_password: '',
       },
+    },
+  }),
 
-      methods: {
-        fetchProfile()
-        {
-          axiosApiInstance.get('auth/user')
-          .then(( response ) => {
-            console.log(response.data);
-            this.initFormProfile(response)
+  computed: {
+    ...mapGetters('auth', {
+      user: 'user',
+    }),
+
+    confirmPasswordRule() {
+      return [
+        (v) => !!v || 'Confirm Password',
+        () =>
+          this.formPassword.data.password ==
+            this.formPassword.data.confirm_password ||
+          'Password does not match',
+      ]
+    },
+  },
+
+  mounted() {
+    this.fetchProfile()
+  },
+
+  methods: {
+    fetchProfile() {
+      axiosApiInstance.get('auth/user').then((response) => {
+        console.log(response.data)
+        this.initFormProfile(response)
+      })
+    },
+
+    initFormProfile(response) {
+      this.formProfile.data = {
+        avatar: {
+          src: response.data.avatar,
+          file: '',
+        },
+        id: response.data.id,
+        first_name: response.data.first_name,
+        last_name: response.data.last_name,
+        username: response.data.username,
+        email: response.data.email,
+      }
+    },
+
+    updateProfile() {
+      if (this.$refs.formProfile.validate()) {
+        this.formProfile.submitting = true
+        let formData = new FormData()
+
+        formData.append('first_name', this.formProfile.data.first_name)
+        formData.append('last_name', this.formProfile.data.last_name)
+        formData.append('username', this.formProfile.data.username)
+        formData.append('email', this.formProfile.data.email)
+        formData.append('_method', 'PUT')
+        if (this.formProfile.data.avatar.src)
+          formData.append('avatar[src]', this.formProfile.data.avatar.src)
+        if (this.formProfile.data.avatar.file)
+          formData.append('avatar[file]', this.formProfile.data.avatar.file)
+        axiosApiInstance
+          .post(
+            `setting/change_profile/${this.formProfile.data.id}`,
+            formData,
+            {
+              headers: { 'content-type': 'multipart/form-data' },
+            }
+          )
+          .then((response) => {
+            // this.$notify({group: 'alert', type: 'success', title: 'Success', text: response.data.message})
+            this.$store.dispatch('pushNotification', {
+              message: response.data.message,
+              type: 'success',
+            })
+            this.fetchProfile()
+            this.$store.dispatch('auth/getUser').catch(() => {})
           })
-        },
+          .catch(({ response }) => {
+            // this.$notify({group: 'alert', type: 'error', title: 'Error', text: response.data.message || 'Cannot connect to the remote server.' })
+            this.$store.dispatch('pushNotification', {
+              message:
+                response.data.message || 'Cannot connect to the remote server.',
+              type: 'error',
+            })
+          })
+          .finally(() => {
+            this.formProfile.submitting = false
+          })
+      }
+    },
 
-        initFormProfile(response)
-        {
-          this.formProfile.data = {
-            avatar: {
-              src: response.data.avatar,
-              file: ''
-            },
-            first_name: response.data.first_name,
-            last_name: response.data.last_name,
-            username: response.data.username,
-            email: response.data.email,
-          }
-        },
-
-        updateProfile()
-        {
-          if (this.$refs.formProfile.validate())
-          {
-            this.formProfile.submitting = true
-            let formData = new FormData()
-
-            formData.append('first_name', this.formProfile.data.first_name)
-            formData.append('last_name', this.formProfile.data.last_name)
-            formData.append('username', this.formProfile.data.username)
-            formData.append('email', this.formProfile.data.email)
-            if (this.formProfile.data.avatar.src) formData.append('avatar[src]', this.formProfile.data.avatar.src)
-            if (this.formProfile.data.avatar.file) formData.append('avatar[file]', this.formProfile.data.avatar.file)
-            axiosApiInstance.post('setting/change_profile', formData, {
-              headers: {'content-type': 'multipart/form-data'}
+    changePassword() {
+      if (this.$refs.formPassword.validate()) {
+        this.$refs.btn_change_password.$el.focus()
+        this.$refs.btn_change_password.$el.blur()
+        this.formPassword.submitting = true
+        axiosApiInstance
+          .put(`setting/change_password/${this.formProfile.id}`, this.formPassword.data)
+          .then((response) => {
+            this.$store.dispatch('pushNotification', {
+              message: response.data.message,
+              type: 'success',
             })
-            .then(( response ) => {
-              this.$notify({group: 'alert', type: 'success', title: 'Success', text: response.data.message})
-              this.fetchProfile()
-              this.$store.dispatch("auth/getUser").catch(() => {})
+            this.$refs.formPassword.reset()
+            // this.$notify({
+            //   group: 'alert',
+            //   type: 'success',
+            //   title: 'Success',
+            //   text: response.data.message,
+            // })
+          })
+          .catch(({ response }) => {
+            // this.$notify({
+            //   group: 'alert',
+            //   type: 'error',
+            //   title: 'Error',
+            //   text:
+            //     response.data.message || 'Cannot connect to the remote server.',
+            // })
+            this.$store.dispatch('pushNotification', {
+              message:
+                response.data.message || 'Cannot connect to the remote server.',
+              type: 'error',
             })
-            .catch(({ response }) => {
-              this.$notify({group: 'alert', type: 'error', title: 'Error', text: response.data.message || 'Cannot connect to the remote server.' })
-            })
-            .finally(() => {
-              this.formProfile.submitting = false
-            })
-          }
-        },
-
-        changePassword()
-        {
-          if (this.$refs.formPassword.validate())
-          {
-            this.$refs.btn_change_password.$el.focus()
-            this.$refs.btn_change_password.$el. blur()
-            this.formPassword.submitting = true
-            axiosApiInstance.post('setting/change_password', this.formPassword.data)
-            .then(( response ) => {
-              this.$refs.formPassword.reset()
-              this.$notify({group: 'alert', type: 'success', title: 'Success', text: response.data.message})
-            })
-            .catch(({ response }) => {
-              this.$notify({group: 'alert', type: 'error', title: 'Error', text: response.data.message || 'Cannot connect to the remote server.' })
-            })
-            .finally(() => {
-              this.formPassword.submitting = false
-            })
-          }
-        },
-
-      },
-  }
-  </script>
+          })
+          .finally(() => {
+            this.formPassword.submitting = false
+          })
+      }
+    },
+  },
+}
+</script>
